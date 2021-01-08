dfad9980d408c793a1e710be8778f7a02a606458
nickmwangemi <nickmwangemi@gmail.com>
2021-01-08 21:13:05 +0300

    Update resource URLs

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
-app.use('/categories', categoriesRouter)
+app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/categories', categoriesRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404))
